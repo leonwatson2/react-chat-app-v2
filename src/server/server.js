ce68c7d1abeda4b073611d8e3543148eb9bf3734
Leon Watson <code@vlw2.com>
2023-04-16 18:33:47 -0500

    adding website to origins

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "http://vlw2.com"]
   }
 });
 app.get('/', (req, res) => {
